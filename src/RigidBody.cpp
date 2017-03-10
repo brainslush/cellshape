7337cf8802a0a194e6caf0b0654f098834abd83d
brainslush <benny@horrorservice.de>
2017-03-10 17:47:44 +0100

    rigid body continued

--- src/RigidBody.cpp
+++ src/RigidBody.cpp
@@ -7,12 +7,48 @@
 
 #include "RigidBody.h"
 
-RigidBody::RigidBody() {
-  // TODO Auto-generated constructor stub
-
+RigidBody::RigidBody(
+  ofVec2d iX,
+  double iR,
+  double iI,
+  double iM
+):
+  X(iX),
+  R(iR),
+  I(iI),
+  M(iM)
+{
+  P = ofVec2d(0,0);
+  L = 0;
+  F = ofVec2d(0,0);
+  T = ofVec2d(0,0);
 }
 
 RigidBody::~RigidBody() {
   // TODO Auto-generated destructor stub
 }
 
+ofVec2d& RigidBody::get_position() {
+  return X;
+}
+double& RigidBody::get_rotation() {
+  return R;
+}
+void RigidBody::add_force(ofVec2d& iX, ofVec2d& iF) {
+  F += iF;
+  ofVec2d d = iX - X;
+  T += d.length() * d.perpendicular().normalize().dot(iF);
+}
+void RigidBody::add_force(double iX, double& iF) {
+  F += iF;
+  T += iX * iF;
+}
+void RigidBody::make_timeStep() {
+  // calc displacement
+  ofVec2d a = F / M;
+  // calc rotation
+
+  // reset forces and torque
+  F.x, F.y = 0;
+  T = 0;
+}
--- src/RigidBody.h
+++ src/RigidBody.h
@@ -5,13 +5,34 @@
  *      Author: siegbahn
  */
 
+#include "variable_type.h"
+
 #ifndef SRC_RIGIDBODY_H_
 #define SRC_RIGIDBODY_H_
 
 class RigidBody {
 public:
-  RigidBody();
+  RigidBody(
+      ofVec2d iX,
+      double iR,
+      double iI,
+      double iM
+  );
   virtual ~RigidBody();
+  virtual ofVec2d& get_position();
+  virtual double& get_rotation();
+  virtual void add_force(ofVec2d& iX, ofVec2d& iF);
+  virtual void add_force(double iX, double& iF);
+  virtual void make_timeStep();
+protected:
+  ofVec2d X; // position
+  double R; // orientation
+  ofVec2d P; // momentum
+  double L; // angular momentum
+  double I; // moment of inertia
+  double M; // mass
+  ofVec2d F; // forces
+  double T; // torque
 };
 
 #endif /* SRC_RIGIDBODY_H_ */
