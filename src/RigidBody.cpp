2491351f31f9684c5794d530416c1653aef09c33
brainslush <benny@horrorservice.de>
2017-03-10 14:32:15 +0100

    added rigid body base

---
+++ src/RigidBody.cpp
@@ -0,0 +1,18 @@
+/*
+ * RigidBody.cpp
+ *
+ *  Created on: Mar 8, 2017
+ *      Author: siegbahn
+ */
+
+#include "RigidBody.h"
+
+RigidBody::RigidBody() {
+  // TODO Auto-generated constructor stub
+
+}
+
+RigidBody::~RigidBody() {
+  // TODO Auto-generated destructor stub
+}
+
---
+++ src/RigidBody.h
@@ -0,0 +1,17 @@
+/*
+ * RigidBody.h
+ *
+ *  Created on: Mar 8, 2017
+ *      Author: siegbahn
+ */
+
+#ifndef SRC_RIGIDBODY_H_
+#define SRC_RIGIDBODY_H_
+
+class RigidBody {
+public:
+  RigidBody();
+  virtual ~RigidBody();
+};
+
+#endif /* SRC_RIGIDBODY_H_ */
