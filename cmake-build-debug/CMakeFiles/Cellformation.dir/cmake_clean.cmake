01bc65246867b37ec1d24bce777e0b2bf55b0fee
brainslush <benny@horrorservice.de>
2017-06-07 21:37:20 +0200

    first it

---
+++ cmake-build-debug/CMakeFiles/Cellformation.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cellformation.dir/src/base.cpp.o"
+  "CMakeFiles/Cellformation.dir/src/cActin.cpp.o"
+  "CMakeFiles/Cellformation.dir/src/cBaseclasses.cpp.o"
+  "CMakeFiles/Cellformation.dir/src/cCell.cpp.o"
+  "CMakeFiles/Cellformation.dir/src/cFac.cpp.o"
+  "CMakeFiles/Cellformation.dir/src/cMembrane.cpp.o"
+  "CMakeFiles/Cellformation.dir/src/cSurface.cpp.o"
+  "CMakeFiles/Cellformation.dir/src/grid.cpp.o"
+  "CMakeFiles/Cellformation.dir/src/gui.cpp.o"
+  "CMakeFiles/Cellformation.dir/src/main.cpp.o"
+  "CMakeFiles/Cellformation.dir/src/ofApp.cpp.o"
+  "CMakeFiles/Cellformation.dir/src/random.cpp.o"
+  "CMakeFiles/Cellformation.dir/src/RigidBody.cpp.o"
+  "CMakeFiles/Cellformation.dir/home/siegbahn/gitprojects/MA/of/addons/ofxDatGui/src/ofxDatGui.cpp.o"
+  "CMakeFiles/Cellformation.dir/home/siegbahn/gitprojects/MA/of/addons/ofxDatGui/src/core/ofxDatGuiComponent.cpp.o"
+  "CMakeFiles/Cellformation.dir/home/siegbahn/gitprojects/MA/of/addons/ofxDatGui/src/libs/ofxSmartFont/ofxSmartFont.cpp.o"
+  "../bin/Cellformation.pdb"
+  "../bin/Cellformation"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cellformation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
