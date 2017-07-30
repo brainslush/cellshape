7fa4d62626d7a7e64a0a28d72eb91d7aa107ddde
brainslush <benni@brainslush.de>
2017-07-30 05:20:06 +0200

    fixed isInBounds funcions

--- src/bmath.h
+++ src/bmath.h
@@ -8,14 +8,22 @@
 namespace bmath {
     // checks if number lies in range between min and max with v € [min,max)
     template<typename T>
-    bool isInBounds(const T &v, const T &min, const T &max) {
-        return !(v < min) && (v < max);
+    bool isInBounds(const T &v, const T &a, const T &b) {
+        if (a < b) {
+            return !(v < a) && (v < b);
+        } else {
+            return !(v < b) && (v < a);
+        }
     }
 
     // checks if number lies in range between min and max with v € [min,max]
     template<typename T>
-    bool isInBoundsC(const T &v, const T &min, const T &max) {
-        return !(v < min) && !(max < v);
+    bool isInBoundsC(const T &v, const T &a, const T &b) {
+        if (a < b) {
+            return !(v < a) && !(b < v);
+        } else {
+            return !(v < b) && !(a < v);
+        }
     }
 };
 
